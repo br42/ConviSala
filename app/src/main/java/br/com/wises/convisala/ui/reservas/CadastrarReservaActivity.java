6651d06bcc8276ed5916ee58c5c78aa8b0f07f44
^ <@>
2020-02-18 17:03:43 -0300

    Correção de bugs

--- app/src/main/java/br/com/wises/convisala/ui/reservas/CadastrarReservaActivity.java
+++ app/src/main/java/br/com/wises/convisala/ui/reservas/CadastrarReservaActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -12,6 +13,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONObject;
 import org.w3c.dom.Text;
+
+import java.util.Date;
 
 import br.com.wises.convisala.Aplicativo;
 import br.com.wises.convisala.R;
@@ -31,18 +34,23 @@ public class CadastrarReservaActivity extends AppCompatActivity {
             public void onClick(View v) {
                 EditText descricaoView = findViewById(R.id.activity_cadastrar_reserva_descricao);
                 EditText salaView = findViewById(R.id.activity_cadastrar_reserva_sala);
+                CalendarView horaInicioView = findViewById(R.id.activity_cadastrar_reserva_hora_inicio);
+                CalendarView horaFimView = findViewById(R.id.activity_cadastrar_reserva_hora_fim);
 
                 String descricao = descricaoView.getText().toString();
                 Integer salaId = 0;
                 try {salaId = Integer.parseInt(salaView.getText().toString());} catch (Exception e) {e.printStackTrace();}
+                long horaInicio = horaInicioView.getDate();
+                long horaFim = horaFimView.getDate();
+
 
                 JSONObject object = new JSONObject();
                 try {
                     object.put("descricao", descricao);
                     object.put("id_sala", salaId);
                     object.put("id_usuario", Aplicativo.gerenciadorLogin.getUsuario().getId());
-                    object.put("data_hora_inicio", 0L);
-                    object.put("data_hora_fim", 0L);
+                    object.put("data_hora_inicio", horaInicio);
+                    object.put("data_hora_fim", horaFim);
                 } catch (Exception e) {e.printStackTrace();}
 
                 String base64 = "";
